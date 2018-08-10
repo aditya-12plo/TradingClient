a27583c4c4a42b8bc12f3f31fe9ea7a8445331f3
nugroho.adty <nugroho.adty@gmail.com>
2018-08-11 01:10:24 +0700

    Initial Code

---
+++ TradingClientApp/TradingClientApp/CTSFixClient.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using QuickFix;
+using QuickFix.Fields;
+using QuickFix.Transport;
+
+namespace TradingClientApp
+{
+    public class CTSFixClient : QuickFix.IApplication
+    {
+
+        IInitiator initiator;
+
+        public event Action<string> OnProgress;
+        /// <summary>
+        /// InitializSession
+        /// </summary>
+        public Task Initialize(Action<string> progress,CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+
+            {
+                var settings = new SessionSettings("cts.cfg");
+
+                IMessageStoreFactory messageFactory = new FileStoreFactory(settings);
+
+                ILogFactory logFactory = new FileLogFactory(settings);
+
+                initiator = new SocketInitiator(this, messageFactory, settings);
+
+                progress("Initialization done");
+
+            }, cancellationToken);
+        }
+
+        public void Connect()
+        {
+            initiator.Start();
+        }
+
+
+        public void Disconnect()
+        {
+            initiator.Stop();
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            if (message is QuickFix.FIX42.Heartbeat)
+            {
+                OnProgress("Heartbeat");
+            }
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            OnProgress("Session created");
+        }
+
+        //if logon is successful
+        public void OnLogon(SessionID sessionID)
+        {
+            OnProgress("Connection is successful");
+           // throw new NotImplementedException();
+        }
+
+        //if logon is failed
+        public void OnLogout(SessionID sessionID)
+        {
+            OnProgress("Connection is loggedout");
+        }
+
+        //hook admin messages before calling server
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            if(message is QuickFix.FIX42.Logon)
+            {
+                var logon = message as QuickFix.FIX42.Logon;
+                logon.SetField(new StringField(553, "FIXAGUS")); //username
+                logon.SetField(new StringField(554, "QWER1234")); //username
+
+            }
+        }
+
+        public void ToApp(Message message, SessionID sessionId)
+        {
+          //  throw new NotImplementedException();
+        }
+    }
+}
---
+++ TradingClientApp/TradingClientApp/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradingClientApp
+{
+    public partial class Form1 : Form
+    {
+        CTSFixClient _ctsFixClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if(_ctsFixClient==null)
+            {
+                _ctsFixClient = new CTSFixClient();
+                _ctsFixClient.OnProgress += ShowProgress;
+            }
+
+           await _ctsFixClient.Initialize(ShowProgress,CancellationToken.None);
+
+            _ctsFixClient.Connect();
+
+        }
+
+        private void ShowProgress(string msg)
+        {
+            if(InvokeRequired)
+            {
+                Invoke(new Action<string>(ShowProgress), msg);
+                return;
+            }
+            richTextBox1.Text +=$"\r\n{msg}";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(_ctsFixClient!=null)
+            {
+                _ctsFixClient.Disconnect();
+            }
+        }
+    }
+}
